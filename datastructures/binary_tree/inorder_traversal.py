66d48dd59a518e915016a714eb952580d6c8cc80
thenakulchawla <msg@nakul.net>
2021-06-07 00:02:51 -0700

    Remove datastructures

--- datastructures/binary_tree/inorder_traversal.py
+++
@@ -1,8 +0,0 @@
-
-def inorder(node, arr):
-    if node is None:
-        return
-
-    inorder(node.left, arr)
-    arr.append(node.val)
-    inorder(node.right, arr)
--- datastructures/binary_tree/node.py
+++
@@ -1,9 +0,0 @@
-
-# A python class that represents an
-# individual node in a binary tree
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
-
